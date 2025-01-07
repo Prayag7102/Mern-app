7f0e96ce2cb27850201ec9fc60472e229cf1746f
Prayag7102 <agravatprayag0@gmail.com>
2025-01-07 13:03:57 +0530

    d

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -39,10 +39,6 @@ const Checkout = () => {
       const base64Url = token.split('.')[1];
       const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
       const decodedToken = JSON.parse(window.atob(base64));
-
-
-      console.log(decodedToken.id);
-      
       
       if (!decodedToken.id) {
         toast.error('Invalid session. Please login again');
