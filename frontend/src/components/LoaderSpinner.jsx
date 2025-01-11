1a760c6d726f4c7a8ff5a5035f16880058038506
Prayag7102 <agravatprayag0@gmail.com>
2025-01-11 10:13:22 +0530

    loader

--- frontend/src/components/LoaderSpinner.jsx
+++ frontend/src/components/LoaderSpinner.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
 
   return (
     <div className="loader-container flex justify-center items-center w-full">
-      <div className="h-1 w-full bg-gray-200">
+      <div className="h-screen w-full bg-gray-200">
         <div className="h-1 bg-[#000080] animate-youtube-loading"></div>
       </div>
     </div>
