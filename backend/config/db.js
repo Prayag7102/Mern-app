70dd08eb18e042ac48716ded238bdd4a50659385
Prayag7102 <agravatprayag0@gmail.com>
2025-01-21 14:43:06 +0530

    live_url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,19 @@
-  const mongoose = require("mongoose");
-
+   const mongoose = require("mongoose");
+  
   const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI);
+      if (process.env.NODE_ENV === 'production') {
+        await mongoose.connect(process.env.MONGO_LIVEURI);
+      } else {
+        await mongoose.connect(process.env.MONGO_URI);
+      }
       console.log("MongoDB Connected");
     } catch (error) {
       console.error(error);
       process.exit(1);
     }
   };
+  
+  module.exports = connectDB;
 
   module.exports = connectDB;
