b3426365dff0a91f54d23e70fe86f37dd43d8ba4
prayag7102 <agravatprayag0@gmail.com>
2025-04-15 15:59:27 +0530

    d

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
                 <button className="flex capitalize items-center text-gray-800 bg-blue-600 dark:text-white hover:bg-blue-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2">
                   <FaUserCircle className=" text-lg" />
                 </button>
-                <div className="absolute right-0 top-5 z-10 mt-2 w-[8rem] bg-white rounded-md shadow-lg hidden group-hover:block">
+                <div className="absolute right-0 top-[28px] z-10 mt-2 w-[8rem] bg-white rounded-md shadow-lg hidden group-hover:block">
                   <ul className="py-1" aria-labelledby="dropdownDefaultButton">
                     <li>
                       <Link
